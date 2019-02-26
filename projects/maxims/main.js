7c8a9257d6e943a357f8bac8243baf4d45f94b23
samsj948 <samsj948@newschool.edu>
2019-02-25 21:00:43 -0500

    Add additional js to a few more elements

--- projects/maxims/main.js
+++ projects/maxims/main.js
@@ -1,4 +1,97 @@
 
+//ALASKA
+// Get all elements with the miraculous class and store it in the variable `miraculousElements`.
+  var alaskaElements = document.querySelectorAll('.alaska');
+  // Loop through each element using `forEach`
+  alaskaElements.forEach(function(element) {
+    // Use `addEventListener` to add a `mouseenter` listener. This will trigger
+    // a function whenever your mouse enters one of the miraculous elements.
+    element.addEventListener('mouseenter', function() {
+      // Loop through each miraculous element again.
+      alaskaElements.forEach(function(element) {
+        // Add an active class to each element.
+        element.classList.add('active');
+      });
+    });
+    // Add another event listener, this case listening for the `mouseleave` event.
+    // This will remove the active class when the user's mouse leaves the element.
+    element.addEventListener('mouseleave', function() {
+      // Loop through each miraculous element again.
+      alaskaElements.forEach(function(element) {
+        // Remove the active class from each element.
+        element.classList.remove('active');
+      });
+    });
+  });
+//AMELIE
+// Get all elements with the miraculous class and store it in the variable `miraculousElements`.
+  var amelieElements = document.querySelectorAll('.amelie');
+  // Loop through each element using `forEach`
+  amelieElements.forEach(function(element) {
+    // Use `addEventListener` to add a `mouseenter` listener. This will trigger
+    // a function whenever your mouse enters one of the miraculous elements.
+    element.addEventListener('mouseenter', function() {
+      // Loop through each miraculous element again.
+      amelieElements.forEach(function(element) {
+        // Add an active class to each element.
+        element.classList.add('active');
+      });
+    });
+    // Add another event listener, this case listening for the `mouseleave` event.
+    // This will remove the active class when the user's mouse leaves the element.
+    element.addEventListener('mouseleave', function() {
+      // Loop through each miraculous element again.
+      amelieElements.forEach(function(element) {
+        // Remove the active class from each element.
+        element.classList.remove('active');
+      });
+    });
+  });
+
+//AQUATIC
+// Get all elements with the miraculous class and store it in the variable `miraculousElements`.
+  var aquaticElements = document.querySelectorAll('.amelie');
+  // Loop through each element using `forEach`
+  amelieElements.forEach(function(element) {
+    // Use `addEventListener` to add a `mouseenter` listener. This will trigger
+    // a function whenever your mouse enters one of the miraculous elements.
+    element.addEventListener('mouseenter', function() {
+      // Loop through each miraculous element again.
+      amelieElements.forEach(function(element) {
+        // Add an active class to each element.
+        element.classList.add('active');
+      });
+    });
+    // Add another event listener, this case listening for the `mouseleave` event.
+    // This will remove the active class when the user's mouse leaves the element.
+    element.addEventListener('mouseleave', function() {
+      // Loop through each miraculous element again.
+      amelieElements.forEach(function(element) {
+        // Remove the active class from each element.
+        element.classList.remove('active');
+      });
+    });
+  });
+
+//BEAUTIFUL
+
+//BEES
+
+//CATCHER
+
+//CLOCKS
+
+//CURIOUS
+
+//EVERLASTING
+
+//GALAPAGOS
+
+//LOBSTER
+
+//MIDNIGHT
+
+//MIRACULOUS
   // Get all elements with the miraculous class and store it in the variable `miraculousElements`.
   var miraculousElements = document.querySelectorAll('.miraculous');
   // Loop through each element using `forEach`
@@ -22,3 +115,21 @@
       });
     });
   });
+
+//MOCKINGBIRD
+
+//NINETEEN
+
+//ROBINSONS
+
+//SLAUGHTERHOUSE
+
+//STREET
+
+//SUNSHINE
+
+//TRUMAN
+
+//UNIVERSE
+
+//ZUCCHINI
