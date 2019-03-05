2977e17433564f53deff727451738140326c0111
samsj948 <samsj948@newschool.edu>
2019-03-05 00:19:28 -0500

    Add animation

---
+++ projects/jsexercise/main.js
@@ -0,0 +1,36 @@
+console.log('Julia');
+
+console.log(2);
+
+console.log('2');
+
+console.log(true);
+
+var firstArray = ['j,u,l,i,a'];
+
+console.log(['j,u,l,i,a']);
+
+console.log([7,'example',false]);
+
+console.log('julia'+'sams');
+
+console.log(75+25);
+
+console.log('j','a');
+
+var superEx = ('supercalifragilisticexpialidocious');
+console.log(superEx.length);
+
+console.log(5==4);
+
+if (students=16) {
+console.log('yes all students are present');
+} else {
+console.log('no, not all students are here');
+}
+
+for (var i = 0; i < firstArray.length; i++ ) {
+console.log(firstArray[i]);
+}
+
+if (time)
