193549eea3ca9f7e19b838f52a9d4ff6b64529b3
samsj948 <samsj948@newschool.edu>
2019-04-24 19:05:02 -0400

    add madlib

--- projects/madlib/main.js
+++ projects/madlib/main.js
@@ -48,22 +48,22 @@ $(function() {
 		$.get(randomPerson, function(data){
 
 			$('.word-adjective-1').html(data.adjective1);
-		$('.word-adjective-2').html(data.adjective2);
-		$('.word-artSupply').html(data.artSupply);
-		$('.word-artistName').html(data.artistName);
-		$('.word-bodyPart').html(data.bodyPart);
-		$('.word-celebrityName').html(data.celebrityName);
-		$('.word-computerSoftware').html(data.computerSoftware);
-		$('.word-foods').html(data.foods.join(', '));
-		$('.word-noun1').html(data.noun1);
-		$('.word-noun2').html(data.noun2);
-		$('.word-numbers').html(data.numbers.join(', '));
-		$('.word-placeInNewYork').html(data.placeInNewYork);
-		$('.word-pluralNoun1').html(data.pluralNoun1);
-		$('.word-pluralNoun2').html(data.pluralNoun2);
-		$('.word-pluralNoun3').html(data.pluralNoun3);
-		$('.word-verbEndingInIng').html(data.verbEndingInIng);
-		$('.word-websiteName').html(data.websiteName);
+			$('.word-adjective-2').html(data.adjective2);
+			$('.word-artSupply').html(data.artSupply);
+			$('.word-artistName').html(data.artistName);
+			$('.word-bodyPart').html(data.bodyPart);
+			$('.word-celebrityName').html(data.celebrityName);
+			$('.word-computerSoftware').html(data.computerSoftware);
+			$('.word-foods').html(data.foods.join(', '));
+			$('.word-noun1').html(data.noun1);
+			$('.word-noun2').html(data.noun2);
+			$('.word-numbers').html(data.numbers.join(', '));
+			$('.word-placeInNewYork').html(data.placeInNewYork);
+			$('.word-pluralNoun1').html(data.pluralNoun1);
+			$('.word-pluralNoun2').html(data.pluralNoun2);
+			$('.word-pluralNoun3').html(data.pluralNoun3);
+			$('.word-verbEndingInIng').html(data.verbEndingInIng);
+			$('.word-websiteName').html(data.websiteName);
 
 	});
 
