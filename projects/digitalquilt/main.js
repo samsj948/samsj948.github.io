80274a3641c0a6272bf3080b0ad3702ddb0f62d8
samsj948 <samsj948@newschool.edu>
2019-04-25 23:46:30 -0400

    Upload poster designs and add to project page

--- projects/digitalquilt/main.js
+++ projects/digitalquilt/main.js
@@ -1,12 +1,26 @@
 $(function(){
 	console.log("digital quilt")
 
-	var url = 'http://api.giphy.com/v1/gifs/search?q=mountains&api_key=aAKKbSeY3anXx460GUleGhjnpfjhOHNA'
+	var getImage = function(term) {
+		var url = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=aAKKbSeY3anXx460GUleGhjnpfjhOHNA&limit=10'
 
-	$.get(url, function(data) {
-		console.log(data);
+		$.get(url, function(data) {
+			console.log(data);
 
+			var img = document.createElement("img");
+			img.src = data.data[0].images.original.url;
 
+			$('.term-' + term).html(img);
+		});
+	};
+
+	$('.button').click(function() {
+		var term = $(this).data('term');
+		console.log(term);
+		getImage(term);
 	});
 
 })
+
+// $('.bar1').html(data.data[0].images.looping.mp4);
+// $('.bar1').html(data.data.images.url);
