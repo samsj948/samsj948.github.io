87dfb63bb7a08e22e3fea1bb1b20070b79cd0fb6
samsj948 <samsj948@newschool.edu>
2019-04-30 11:42:31 -0400

    Digital quilt add js for multiple gifs

--- projects/digitalquilt/main.js
+++ projects/digitalquilt/main.js
@@ -22,5 +22,225 @@ $(function(){
 
 })
 
-// $('.bar1').html(data.data[0].images.looping.mp4);
-// $('.bar1').html(data.data.images.url);
+$(function(){
+	console.log("digital quilt")
+
+	var getImage = function(term) {
+		var url = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=aAKKbSeY3anXx460GUleGhjnpfjhOHNA&limit=10'
+
+		$.get(url, function(data) {
+			console.log(data);
+
+			var img = document.createElement("img");
+			img.src = data.data[1].images.original.url;
+
+			$('.term1-' + term).html(img);
+		});
+	};
+
+	$('.button').click(function() {
+		var term = $(this).data('term');
+		console.log(term);
+		getImage(term);
+	});
+
+})
+
+$(function(){
+	console.log("digital quilt")
+
+	var getImage = function(term) {
+		var url = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=aAKKbSeY3anXx460GUleGhjnpfjhOHNA&limit=10'
+
+		$.get(url, function(data) {
+			console.log(data);
+
+			var img = document.createElement("img");
+			img.src = data.data[2].images.original.url;
+
+			$('.term2-' + term).html(img);
+		});
+	};
+
+	$('.button').click(function() {
+		var term = $(this).data('term');
+		console.log(term);
+		getImage(term);
+	});
+
+})
+
+$(function(){
+	console.log("digital quilt")
+
+	var getImage = function(term) {
+		var url = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=aAKKbSeY3anXx460GUleGhjnpfjhOHNA&limit=10'
+
+		$.get(url, function(data) {
+			console.log(data);
+
+			var img = document.createElement("img");
+			img.src = data.data[3].images.original.url;
+
+			$('.term3-' + term).html(img);
+		});
+	};
+
+	$('.button').click(function() {
+		var term = $(this).data('term');
+		console.log(term);
+		getImage(term);
+	});
+
+})
+
+$(function(){
+	console.log("digital quilt")
+
+	var getImage = function(term) {
+		var url = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=aAKKbSeY3anXx460GUleGhjnpfjhOHNA&limit=10'
+
+		$.get(url, function(data) {
+			console.log(data);
+
+			var img = document.createElement("img");
+			img.src = data.data[4].images.original.url;
+
+			$('.term4-' + term).html(img);
+		});
+	};
+
+	$('.button').click(function() {
+		var term = $(this).data('term');
+		console.log(term);
+		getImage(term);
+	});
+
+})
+
+$(function(){
+	console.log("digital quilt")
+
+	var getImage = function(term) {
+		var url = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=aAKKbSeY3anXx460GUleGhjnpfjhOHNA&limit=10'
+
+		$.get(url, function(data) {
+			console.log(data);
+
+			var img = document.createElement("img");
+			img.src = data.data[5].images.original.url;
+
+			$('.term5-' + term).html(img);
+		});
+	};
+
+	$('.button').click(function() {
+		var term = $(this).data('term');
+		console.log(term);
+		getImage(term);
+	});
+
+})
+
+$(function(){
+	console.log("digital quilt")
+
+	var getImage = function(term) {
+		var url = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=aAKKbSeY3anXx460GUleGhjnpfjhOHNA&limit=10'
+
+		$.get(url, function(data) {
+			console.log(data);
+
+			var img = document.createElement("img");
+			img.src = data.data[6].images.original.url;
+
+			$('.term6-' + term).html(img);
+		});
+	};
+
+	$('.button').click(function() {
+		var term = $(this).data('term');
+		console.log(term);
+		getImage(term);
+	});
+
+})
+
+$(function(){
+	console.log("digital quilt")
+
+	var getImage = function(term) {
+		var url = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=aAKKbSeY3anXx460GUleGhjnpfjhOHNA&limit=10'
+
+		$.get(url, function(data) {
+			console.log(data);
+
+			var img = document.createElement("img");
+			img.src = data.data[7].images.original.url;
+
+			$('.term7-' + term).html(img);
+		});
+	};
+
+	$('.button').click(function() {
+		var term = $(this).data('term');
+		console.log(term);
+		getImage(term);
+	});
+
+})
+
+$(function(){
+	console.log("digital quilt")
+
+	var getImage = function(term) {
+		var url = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=aAKKbSeY3anXx460GUleGhjnpfjhOHNA&limit=10'
+
+		$.get(url, function(data) {
+			console.log(data);
+
+			var img = document.createElement("img");
+			img.src = data.data[8].images.original.url;
+
+			$('.term8-' + term).html(img);
+		});
+	};
+
+	$('.button').click(function() {
+		var term = $(this).data('term');
+		console.log(term);
+		getImage(term);
+	});
+
+})
+
+$(function(){
+	console.log("digital quilt")
+
+	var getImage = function(term) {
+		var url = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=aAKKbSeY3anXx460GUleGhjnpfjhOHNA&limit=10'
+
+		$.get(url, function(data) {
+			console.log(data);
+
+			var img = document.createElement("img");
+			img.src = data.data[9].images.original.url;
+
+			$('.term9-' + term).html(img);
+		});
+	};
+
+	$('.button').click(function() {
+		var term = $(this).data('term');
+		console.log(term);
+		getImage(term);
+	});
+
+})
+
+
+
+// for (i = 0; i < data.data.length; i++) {
+// 				var img = document.createElement("img");
+// 				img.src = data.data[i].images.original.url;	
+// 			}
